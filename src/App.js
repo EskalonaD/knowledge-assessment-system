4be71816e07088c17ce36a0bcb9d0802fe883068
EskalonaD <eskalonad@gmail.com>
2019-07-19 17:59:20 +0300

    insert global styles: change dev webpack config & create folder for globa css file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./globalStyles/index.css";
 
 import Header from "@containers/Header";
 import Slider from "@containers/Slider";
--- webpack.dev.js
+++ webpack.dev.js
@@ -42,7 +42,7 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: ["css-loader"]
+        use: ["style-loader", "css-loader"]
       },
       {
         test: /\.scss$/,

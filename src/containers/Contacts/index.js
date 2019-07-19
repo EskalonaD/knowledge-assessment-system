2bbc30918de6a051e34f368039216b6b50f049f3
EskalonaD <eskalonad@gmail.com>
2019-07-19 15:32:45 +0300

    update app structure

--- src/containers/Contacts/index.js
+++ src/containers/Contacts/index.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 
 export default class Contacts extends Component {
   render() {
-    return <div>Contacts;</div>;
+    return (
+    <div>
+      Contacts;
+      <section>Карта</section>
+    </div>;)
   }
 }
--- src/containers/MainPage/index.js
+++ src/containers/MainPage/index.js
@@ -2,6 +2,14 @@ import React, { Component } from "react";
 
 export default class MainPage extends Component {
   render() {
-    return <div>MainPage;</div>;
+    return (
+      <div>
+        <div>
+          <section>Breadcrumbs</section>
+          <section>Поиск</section>
+        </div>
+        MainPage;
+      </div>
+    );
   }
 }

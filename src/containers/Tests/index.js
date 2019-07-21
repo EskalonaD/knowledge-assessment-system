09ad37ffd042f8a7d094ddf43040f1f3802398e8
EskalonaD <eskalonad@gmail.com>
2019-07-21 22:08:40 +0300

    create basic logic

--- src/containers/Tests/index.js
+++ src/containers/Tests/index.js
@@ -1,13 +1,36 @@
 import React, { Component } from "react";
 import styles from "./index.scss";
 import ToolsContainer from "@containers/ToolsContainer";
+import data from "../../dataBase/test.json";
 
 export default class index extends Component {
+  state = {
+    selectedTest: null
+  };
+
   render() {
+    console.log(data);
     return (
       <main>
         <ToolsContainer />
-        TestsPage;
+        {this.state.selectedTest === null ? (
+          <div>
+            {data.map((el, i) => {
+              return (
+                <section
+                  key={el.name}
+                  onClick={() => {
+                    this.setState({ selectedTest: i });
+                  }}
+                >
+                  {el.name}
+                </section>
+              );
+            })}
+          </div>
+        ) : (
+          <div>Test1</div>
+        )}
       </main>
     );
   }

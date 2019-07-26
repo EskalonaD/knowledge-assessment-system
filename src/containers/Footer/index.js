725e8fe88ab3cb4b19814e8bd4af38258365b739
EskalonaD <eskalonad@gmail.com>
2019-07-26 03:17:09 +0300

    create basic footer markup

--- src/containers/Footer/index.js
+++ src/containers/Footer/index.js
@@ -5,10 +5,11 @@ export default class Footer extends Component {
   render() {
     return (
       <footer className={styles.footer}>
-        <section>Копирайт</section>
-        <section>Дата</section>
-        <section>Линк на About Us</section>
-        <section>Контакты</section>
+        <section className={styles.footer__block}>Копирайт</section>
+        <section className={styles.footer__block}>Дата</section>
+        <section className={styles.footer__block}>Линк на About Us</section>
+        <section className={styles.footer__block}>Контакты</section>
+        <section className={styles.footer__block}>Соцсети</section>
       </footer>
     );
   }

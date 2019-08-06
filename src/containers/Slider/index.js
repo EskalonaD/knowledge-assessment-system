53b2d6efa681b76414ab52714c9d3286a65060ae
EskalonaD <eskalonad@gmail.com>
2019-08-07 00:46:51 +0300

    create assets folder/fill it with imgs

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
 import styles from "./index.scss";
+// import mainPagePicture from "@assets/img/img_temp_startpage.jpg";
+
+const pic = require('../../assets/img/img_temp_startpage.jpg')
 
 export default class Slider extends Component {
   render() {
-    return <section className={styles.slider__wrapper} />;
+    return <section className={styles.slider__wrapper}>
+      <img src={pic} />
+    </section>;
   }
 }
